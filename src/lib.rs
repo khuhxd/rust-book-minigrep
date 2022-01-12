10adc49b8d70ab4aebf49497339fbc4b0dc2e165
Ratibor Siryi <ratibor.siryi@gmail.com>
2022-01-12 10:32:45 +0300

    Refactoring search with iterators.
    Also added a little fix to the Config::new method.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
-pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
     let results = if config.case_sensitive {
-	search(&config.query, &contents)
+        search(&config.query, &contents)
     } else {
-	search_case_insensitive(&config.query, &contents)
+        search_case_insensitive(&config.query, &contents)
     };
 
     for line in results {
-	println!("{}", line);
+        println!("{}", line);
     }
 
     Ok(())
@@ -26,53 +26,43 @@ pub struct Config {
 
 impl Config {
     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-	if args.len() < 3 {
-	    return Err("not enough arguments");
-	}
-	
-	let query = match args.next() {
-	    Some(arg) => arg,
-	    None => return Err("Didn't get a query string"),
-	};
-	
-	let filename = match args.next() {
-	    Some(arg) => arg,
-	    None => return Err("Didn't get a file name"),
-	};
-	
-	let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        args.next();
 
-	Ok(Config {
-	    query,
-	    filename,
-	    case_sensitive,
-	})
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-	if line.contains(query) {
-	    results.push(line);
-	}
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
-pub fn search_case_insensitive<'a>(
-    query: &str,
-    contents: &'a str
-) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
 
     for line in contents.lines() {
-	if line.to_lowercase().contains(&query) {
-	    results.push(line);
-	}
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
     }
 
     results
@@ -83,28 +73,28 @@ mod tests {
 
     #[test]
     fn one_result() {
-	let query = "duct";
-	let contents = "\
+        let query = "duct";
+        let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
 Duct tape.";
 
-	assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
     fn case_insensitive() {
-	let query = "rUsT";
-	let contents = "\
+        let query = "rUsT";
+        let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
 Trust me.";
 
-	assert_eq!(
-	    vec!["Rust:", "Trust me."],
-	    search_case_insensitive(query, contents)
-	);
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
